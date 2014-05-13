59f08d482757fd31c22f19d77079c4f7d6da9030
Jeff Lee <shihcl@uci.edu>
2014-05-13 16:55:18 -0700

    Ready to GAE datastore

--- GroupStatus Server/src/edu/uci/ics/luci/groupstatusserver/GroupStatus_ServerServlet.java
+++ GroupStatus Server/src/edu/uci/ics/luci/groupstatusserver/GroupStatus_ServerServlet.java
@@ -3,6 +3,9 @@ package edu.uci.ics.luci.groupstatusserver;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -12,16 +15,31 @@ public class GroupStatus_ServerServlet extends HttpServlet {
 
 		PrintWriter out = resp.getWriter();
 		resp.setContentType("text/plain");
+		
+		System.out.println("line 19 @ GroupStatus_ServerServlet");
 
 		String function = req.getParameter("function");
+		if(function == null) function = "null";
 
 		switch (function) {
 		case "login":
 			String userID = req.getParameter("userID");
 			String userPW = req.getParameter("userPW");
-			if (userID.equals("user001") && userPW.equals("1234"))
+			if (userID.equals("user001") && userPW.equals("1234")){
 				out.println("success");
-			break;
+				
+				ServletContext context = this.getServletContext();
+				RequestDispatcher dispatcher = context.getRequestDispatcher("/new");
+				try {
+//					HttpServletRequest request;
+//					HttpServletResponse response;
+					dispatcher.forward(req, resp);
+				} catch (ServletException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+						break;
 		case "upload":
 			String status = req.getParameter("status");
 			if (status.equals("Coding in Vista del Campo"))
